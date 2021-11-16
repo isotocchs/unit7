54b3fde4b53034091caccbd69d6ebda902fd75bc
IgnacioSoto <isoto@columbushs.com>
2021-11-16 07:27:26 -0500

    save

---
+++ unit7/src/com/columbushs/unit7/Unit7Main.java
@@ -0,0 +1,11 @@
+package com.columbushs.unit7;
+
+public class Unit7Main {
+
+	public static void main(String[] args) {
+
+		Unit7_1IntroArrayLists introObj = new Unit7_1IntroArrayLists();
+
+	}
+
+}
---
+++ unit7/src/com/columbushs/unit7/Unit7_1IntroArrayLists.java
@@ -0,0 +1,23 @@
+package com.columbushs.unit7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Unit7_1IntroArrayLists {
+	
+	//Arrays are static in size - you cannot change the size after they are created
+	
+	//ArrayLists are dynamic in size - you can change the size any time after creation
+	
+	//ArrayLists are designed to store object data not primitives(Int,double,boolean,etc)
+
+	//Need import statement.
+	
+	
+	
+	
+	
+	
+	
+}
