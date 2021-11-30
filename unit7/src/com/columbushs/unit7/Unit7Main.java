43d1a8c0b6c852bbd7ce114c5f8e14b2f880cb29
IgnacioSoto <isoto@columbushs.com>
2021-11-30 09:22:19 -0500

    save

--- unit7/src/com/columbushs/unit7/Unit7Main.java
+++ unit7/src/com/columbushs/unit7/Unit7Main.java
@@ -5,7 +5,7 @@ public class Unit7Main {
 	public static void main(String[] args) {
 
 		Unit7_1IntroArrayLists introObj = new Unit7_1IntroArrayLists();
-		
+		Unit7_2ArrayListMethods arrayListMethods = new Unit7_2ArrayListMethods();
 
 	}
 	
--- unit7/src/com/columbushs/unit7/Unit7_2ArrayListMethods.java
+++ unit7/src/com/columbushs/unit7/Unit7_2ArrayListMethods.java
@@ -1,5 +1,18 @@
 package com.columbushs.unit7;
 
+import java.util.ArrayList;
+
 public class Unit7_2ArrayListMethods {
+	
+	
+	public void arrayMethods() {
+		
+	ArrayList<Integer> a1 =  new ArrayList<Integer>();
+	
+	
+	
+	
+	
+	}
 
 }
