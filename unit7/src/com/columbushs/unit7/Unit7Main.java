72df7b5ee4b44b3dd5f55a1cd1fdb1b220d70e97
IgnacioSoto <isoto@columbushs.com>
2021-12-01 07:55:04 -0500

    save

--- unit7/src/com/columbushs/unit7/Unit7Main.java
+++ unit7/src/com/columbushs/unit7/Unit7Main.java
@@ -1,5 +1,7 @@
 package com.columbushs.unit7;
 
+import java.util.ArrayList;
+
 public class Unit7Main {
 
 	public static void main(String[] args) {
@@ -11,8 +13,28 @@ public class Unit7Main {
 		//arrayListMethods.arrayMethods();
 	
 		Unit7_3ArrayListTraversing arrayListTrav = new Unit7_3ArrayListTraversing();
-		arrayListTrav.arrayListTrav();
+		//arrayListTrav.arrayListTrav();
 		
+		ArrayList<Integer> arrayListInts =  new ArrayList<Integer>();
+		arrayListInts.add(10);
+		arrayListInts.add(45);
+		arrayListInts.add(3);
+		arrayListInts.add(14);
+		arrayListInts.add(567);
+		arrayListInts.add(345);
+		
+		ArrayList<String> arrayListStr = new ArrayList<String>();
+		arrayListStr.add("Apple");
+		arrayListStr.add("Orange");
+		arrayListStr.add("Banana");
+		arrayListStr.add("Grapes");
+		arrayListStr.add("Mango");
+		arrayListStr.add("Berry");
+		
+		Unit7_5Searching searching = new Unit7_5Searching();
+		System.out.println(searching.whereIsTheNumber(3, arrayListInts));
+		
+		System.out.println(searching.whereIsTheString("Grapes", arrayListStr));
 		
 
 	}
--- unit7/src/com/columbushs/unit7/Unit7_4ArrayListAlgorithms.java
+++ unit7/src/com/columbushs/unit7/Unit7_4ArrayListAlgorithms.java
@@ -1,5 +1,66 @@
 package com.columbushs.unit7;
 
+import java.util.ArrayList;
+
 public class Unit7_4ArrayListAlgorithms {
+	
+	public double findMaxArray(double [] arrayIn) {
+		
+		double maxValue = arrayIn[0];
+		
+		for (int i=1;i<arrayIn.length;i++) {
+			
+			if(arrayIn[i]>maxValue) {
+				maxValue = arrayIn[i];
+			}	
+		}		
+		return maxValue;
+	}
+	
+	private double findMaxArrayList(ArrayList<Double> arrayListName) {
+		
+		double max = arrayListName.get(0);
+		
+		for(int index = 1; index<arrayListName.size();index++) {
+			
+			if(arrayListName.get(index)>max) {
+				max=arrayListName.get(index);				
+			}
+		}
+		
+		return max;
+		
+	}
+	
+	
+	public double findMinArray(double [] arrayIn) {
+		
+		double minValue = Double.MAX_VALUE;
+		
+		for (double currentValue:arrayIn) {
+			
+			if(currentValue<minValue) {
+				
+				minValue = currentValue;
+			}
+		}
+		return minValue;
+	}
+	
+	public double findMinArrayList(ArrayList<Double> arrayListIn) {
+		
+		double minValue = Double.MAX_VALUE;
+		
+		for (double currentValue:arrayListIn) {
+			
+			if(currentValue<minValue) {
+				
+				minValue = currentValue;
+			}
+		}
+		return minValue;
+	}
+	
+	
 
 }
